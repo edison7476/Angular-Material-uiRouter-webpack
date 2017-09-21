cc9b45704cfeb2c58642cbc921b81adbaa9dd42a
Edison Wang <edison7476@hotmail.com>
2017-09-20 21:51:43 -0700

    Highcharts added

--- client/app/app.js
+++ client/app/app.js
@@ -1,26 +1,27 @@
 import angular from 'angular';
 import angularAnimate from 'angular-animate';
 import angularMaterial from 'angular-material';
+import Highcharts from 'highcharts/highstock';
+import HighchartsMap from 'highcharts/modules/map';
 import uiRouter from '@uirouter/angularjs';
 
 import AppComponent from './app.component';
-
 import Components from './components/components.module';
 import Commons from './common/common.module';
 import Core from './core/core.module';
-// import './components/components.module';
-// import './core/core.module';
 
 import 'angular-material/angular-material.css';
 import 'normalize.css';
+
+// For Highcharts
+HighchartsMap(Highcharts);// Load Highmaps as a module
+window.Highcharts = Highcharts; //attach Highcharts to global scope
 
 angular.module('app', [
     angularMaterial,
     angularAnimate,
     uiRouter,
-    
-    // 'app.components',
-    // 'app.core',
+
     Components,
     Commons,
     Core,
